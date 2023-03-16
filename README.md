bd321aa61cb780b7905834716aa61085cb91c09b
misha <dedovmu@gmail.com>
2023-03-16 14:49:20 +0300

    Project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## Solid log
+
+Login form made with solid.js and other things
+
+tailwind
+prettier + tailwind plugin
+eslint + typescript rules
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js'
+
+const App: Component = () => {
+    return (
+        <div class='flex h-screen w-screen content-center justify-center'>
+            wow
+        </div>
+    )
+}
+
+export default App
